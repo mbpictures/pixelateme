9e803d3b675587ccb9044d6800e3a0fc056564e8
Marius Butz <mbpictures13@gmail.com>
2023-02-27 20:33:27 +0100

    set up project

---
+++ README.md
@@ -0,0 +1,2 @@
+# PixelateMe
+A Python CLI-Tool and package to pixelate or blur faces in images and videos.
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import versioneer
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read("README.md")
+
+setup(
+    name='PixelateMe',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author='Marius Butz',
+    description='A Python CLI-Tool and package to pixelate or blur faces in images and videos.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mbpictures/pixelateme',
+    packages=find_packages(),
+    entry_points={'console_scripts': [
+        'pixelateme = pixelateme.cli:main',
+    ]},
+    package_data={'pixelateme': ['centerface.onnx']},
+    include_package_data=True,
+    install_requires=[
+        'imageio',
+        'imageio-ffmpeg',
+        'numpy',
+        'tqdm',
+        'scikit-image',
+        'opencv-python',
+    ],
+    extras_require={
+        'gpu':  ['onnxruntime-gpu'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
