668c346be08c84c39848a6f8256ef0707f4af04b
Marius Butz <mbpictures13@gmail.com>
2023-02-27 22:40:14 +0100

    implement basic blur class

---
+++ pixelateme/blur.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+
+
+class Blur:
+    def __init__(self, mode="blur", ellipse=True):
+        self.mode = mode
+        self.ellipse = ellipse
+
+    @staticmethod
+    def get_ellipse(box):
+        x1, y1, x2, y2 = box
+        center = (x2 - x1) // 2, (y2 - y1) // 2
+        size = x2 - x1, y2 - y1
+        return center, size
+
+    @staticmethod
+    def get_ellipse_masks(shape, boxes):
+        mask = np.zeros(shape, dtype="uint8")
+        for box in boxes:
+            center, size = Blur.get_ellipse(box)
+            cv2.ellipse(mask, center, size, 0, 0, 360, (255, 255, 255), -1)
+        return mask
+
+    @staticmethod
+    def get_box_masks(shape, boxes):
+        mask = np.zeros(shape, dtype="uint8")
+        for box in boxes:
+            x1, y1, x2, y2 = box
+            cv2.rectangle(mask, (x1, y1), (x2, y2), (255, 255, 255), -1)
+        return mask
+
+    def get_mask(self, shape, boxes):
+        if self.ellipse:
+            return Blur.get_ellipse_masks(shape, boxes)
+        return Blur.get_box_masks(shape, boxes)
+
+    def solid(self, image, boxes):
+        mask = self.get_mask(image.shape, boxes)
+        zeros = np.zeros(image.shape, dtype="uint8")
+        return np.where(mask > 0, zeros, image)
+
+    def blur(self, image, boxes):
+        blurred = cv2.medianBlur(image, 40)
+        mask = self.get_mask(image.shape, boxes)
+        return np.where(mask > 0, blurred, image)
+
+    def pixelate(self, image, boxes):
+        mask = self.get_mask(image.shape, boxes)
+        w, h = (16, 16)
+        height, width = image.shape[:2]
+        temp = cv2.resize(image, (width / w, height / h), interpolation=cv2.INTER_LINEAR)
+        pixelate = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
+
+        return np.where(mask > 0, pixelate, image)
+
+    def blur_faces(self, image, boxes):
+        if self.mode == "color":
+            return self.solid(image, boxes)
+        if self.mode == "blur":
+            return self.blur(image, boxes)
+        if self.mode == "pixelate":
+            return self.pixelate(image, boxes)
+
+        raise Exception("Unsupported mode. Supported modes are color, blur and pixelate!")
