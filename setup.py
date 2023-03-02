9ffb33a4e63e39c918281cd71a05dccd8c323ed1
Marius Butz <mbpictures13@gmail.com>
2023-03-02 14:49:19 +0100

    fix encoding for long description

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import versioneer
 
-with open('README.md', 'r') as fh:
+with open('README.md', 'r', encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
